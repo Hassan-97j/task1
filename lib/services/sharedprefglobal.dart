197e619414858b4811237942d24452817cf0c767
Hassaan Junaid <hassaanjunaid.soomro@gmail.com>
2022-09-07 09:56:38 -0500

    commit

--- lib/services/sharedprefglobal.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class App {
-//   static SharedPreferences? localStorage;
-  
-//   static Future init() async {
-//     localStorage = await SharedPreferences.getInstance();
-//   }
-// }
